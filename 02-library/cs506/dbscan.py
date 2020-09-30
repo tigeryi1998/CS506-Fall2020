2509514df92cc92fc1879983dce952fe406e4c12
Tiger Yi <tigeryi@bu.edu>
2020-09-30 16:46:49 -0400

    add dbscan

--- 02-library/cs506/dbscan.py
+++ 02-library/cs506/dbscan.py
@@ -25,33 +25,30 @@ class DBC():
                 assignments[NextP] = assignment
             
             if assignments[NextP] == 0:
+                assignments[NextP] = assignment
                 NextPNeighborhood = self._get_neighborhood(NextP)
         
                 if len(NextPNeighborhood) >= self.min_pts:
                     PNeighborhood += NextPNeighborhood
-            
-                assignments[NextP] = assignment
-            
 
         return assignments
 
     def dbscan(self):
         assignments = [0 for _ in range (len (self.dataset) ) ]
-        assignment = 1
+        assignment = 0
 
         for p in range (len (self.dataset) ):
             if assignments[p] != 0:
                 continue
             
             PNeighborhood = self._get_neighborhood(p)
+            
             if len(PNeighborhood) >= self.min_pts:
-                assignment += 1
                 # core point
+                assignment += 1
                 assignments = self._explore_PNeighborhood(assignments, assignment, p, PNeighborhood)
             else:
                 # border or noise point
                 assignments[p] = -1
-            
-
         return assignments
         
